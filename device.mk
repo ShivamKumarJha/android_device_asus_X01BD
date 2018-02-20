b7853cd8bad333802584d82264ae02edd2747f9b
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    X01BD: Build LiveDisplay HAL
    Change-Id: I1a1f630bd8c4670b3fe9c16d2493019f0aa56a95

--- device.mk
+++ device.mk
@@ -245,6 +245,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
