9f19679f3532b6124d1376099b0be1cc7ff54f90
ShivamKumarJha <jha.shivam3@gmail.com>
2019-01-08 02:52:22 +0530

    X01BD: Enable Camera HAL3
    Change-Id: I423f9fb7e34183fdd4324986643902d96d46e93f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,7 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.camera.expose.aux=1 \
-	persist.camera.HAL3.enabled=0 \
+	persist.camera.HAL3.enabled=1 \
 	persist.camera.preview.ubwc=0 \
 	persist.ts.rtmakeup=1 \
 	vendor.camera.aux.packagelist=org.codeaurora.snapcam
